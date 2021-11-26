200c9327a6ad9a79bf4c678b249291f74b1a0d16
H4mb01 <manuel@muenstedt.de>
2021-11-26 13:03:52 +0100

    nav

---
+++ src/components/Nav.vue
@@ -0,0 +1,42 @@
+<template>
+    <div id="nav">
+      <router-link to="/">Home</router-link> 
+      <router-link to="/blog" >Blog</router-link>
+      <router-link to="/css" >CSS</router-link>
+      <router-link to="/dies" >Dies</router-link>
+      <router-link to="/das" >Das</router-link>
+      <router-link to="/about">About</router-link>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Nav",
+}
+</script>
+
+<style>
+
+</style>
+
+<style scoped>
+#nav {
+  position: absolute;
+  top: 0;
+  width: 100%;
+  height: 40px;
+  background-color: #eee;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+  padding: 10px;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+  background: #ddd;
+  display: inline-block;
+}
+</style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,14 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Über mich</h1>
+    <p>
+      Mein Name ist Manu, und ich spiele hier nur rum :)
+    </p>
   </div>
 </template>
+
+<script>
+export default {
+  name: "About",
+}
+</script>
