f16b4debdd21ea0660447722f7db81a3db948b70
H4mb01 <manuel@muenstedt.de>
2021-11-26 12:12:55 +0100

    dist folder

---
+++ dist/precache-manifest.e67e622a598a262fde9a1db8869480e8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56c15e87027c90911493",
+    "url": "/css/app.8d23bc70.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "93db9be330872b5f39c42166d3d25d37",
+    "url": "/index.html"
+  },
+  {
+    "revision": "95b4a16eb33d78fc3030",
+    "url": "/js/about.c3d1aa59.js"
+  },
+  {
+    "revision": "56c15e87027c90911493",
+    "url": "/js/app.53ea2470.js"
+  },
+  {
+    "revision": "f2f4118baaaa8af2b656",
+    "url": "/js/chunk-vendors.dd9342be.js"
+  },
+  {
+    "revision": "3349528f29eba58e787ce104c210a019",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
